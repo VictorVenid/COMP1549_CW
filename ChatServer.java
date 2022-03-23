519be8e90970232a289e386d935c394057904b1d
Victor Dinev <79041921+VictorVenid@users.noreply.github.com>
2022-03-23 16:58:38 +0000

    Coordinator changes upon leaving (lines: 127-130)

--- ChatServer.java
+++ ChatServer.java
@@ -124,6 +124,10 @@ public class ChatServer {
                         DateTimeFormatter hhmm = DateTimeFormatter.ofPattern("HH:mm");
                         LocalTime time = LocalTime.now();
                         writer.println("MESSAGE " + name + " has left (" + time.format(hhmm) + ")");
+                        if (name == coordinator) {
+                            coordinator = names.iterator().next();
+                            writer.println("MESSAGE " + coordinator + " is the new coordinator (" + time.format(hhmm) + ")");
+                        }
                     }
                 }
                 try { socket.close(); } catch (IOException e) {}
